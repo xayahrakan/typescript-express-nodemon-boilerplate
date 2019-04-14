e4d10b08b5cf55c21cabbc158f064d71d801fb80
xayahrakan <49125651+xayahrakan@users.noreply.github.com>
2019-04-14 13:36:17 +0200

    asdasd

--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
      console.log(`Server is running in http://localhost:${PORT}`)
      console.log("test gpg")
+     console.log("asdasdasd")
 })
